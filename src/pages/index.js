e143175de1327929aba1ce7833e95bfcd61495ee
anisarif <83428876+anisarif@users.noreply.github.com>
2022-03-01 00:16:24 +0100

    cs

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,11 @@ import { StaticImage } from 'gatsby-plugin-image'
 // markup
 const IndexPage = () => {
   return (
-    <main className=" bg-black h-full flex items-center">
+    <main className="flex justify-end h-screen bg-black">
       <title>Home Page</title>
-        <StaticImage className=" px-5 m-4 " src={"../images/Otaku2.png"}/>
+      <h1 className="text-white text-2xl pt-40 pr-40"> Welcome to Otaku Basho</h1>
+
+        <StaticImage className="flex h-screen px-2 m-4 " src={"../images/Otaku2.png"}/>
     </main>
   )
 }
