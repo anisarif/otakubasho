08bbb06c553f722eacf083baeba09cdaa8e996a5
anisarif <83428876+anisarif@users.noreply.github.com>
2022-03-01 00:35:46 +0100

    1.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,14 @@ const IndexPage = () => {
   return (
     <main className="flex justify-end h-screen bg-black">
       <title>Home Page</title>
-      <h1 className="text-white text-2xl pt-40 pr-40"> Welcome to Otaku Basho</h1>
+      <div className="justify-center p-40">
+      <h1 className="text-white text-4xl pt-40 pr-40"> Welcome to Otaku Basho</h1>
+      <h2 className=" text-pink-300 text-2xl pt-10 pr-40"> The first Web3 anime streaming plateform</h2>
+      <h1 className="text-white text-4xl pt-40 pr-40"> Coming Soon...</h1>
 
-        <StaticImage className="flex h-screen px-2 m-4 " src={"../images/Otaku2.png"}/>
+
+      </div>
+        <StaticImage className="flex h-screen px-2 m-4 " src={"../images/Otaku2.png"} alt="otaku girl"/>
     </main>
   )
 }
