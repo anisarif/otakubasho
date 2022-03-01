5af617c5adc768b30c8683c719209acb0ca0c409
anisarif <83428876+anisarif@users.noreply.github.com>
2022-03-01 02:33:55 +0100

    v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,17 @@ const IndexPage = () => {
     <main className="flex justify-end h-screen bg-black px-40">
       <title>Home Page</title>
       <div className="justify-center p-40">
-      <h1 className="text-white text-4xl pt-40 p-30"> Welcome to Otaku Basho</h1>
-      <strong className="text-white text-2xl pt-20 pr-40">
+      <h1 className="text-white  text-bold text-4xl pt-40 p-25"> Welcome to Otaku Basho</h1>
+      <div className="text-white text-2xl py-5">
 
       <TypeWriterEffect 
-        startDelay={2000}
+        startDelay={1000}
         cursorColor="#FFC0CB"
         multiText={[
           "The first Web3 Anime Plateform",
           "By Otakus, For Otakus",
-          "Decentralised and Owned by the community",
+          "Decentralised",
+          "Owned by the community",
           "Let's build it together !",
 
         ]}
@@ -28,7 +29,7 @@ const IndexPage = () => {
         multiTextDelay={1000}
         typeSpeed={30}
         />
-      </strong>
+      </div>
       </div>
         <StaticImage className="flex h-screen px-2 m-4 " src={"../images/Otaku2.png"} alt="otaku girl"/>
     </main>
