2a0feb7f75264f813476fa0ec4a8a952fcb910fe
anisarif <83428876+anisarif@users.noreply.github.com>
2022-03-05 18:08:37 +0100

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,11 @@ import TypeWriterEffect from 'react-typewriter-effect'
 
 
 
-// markup
 const IndexPage = () => {
   return (
-    <main className="flex justify-around h-screen bg-black px-40">
+    <main className="flex justify-evenly h-full bg-black px-40">
       <title>Home Page</title>
-      <div className="p-20 pt-40">
+      <div className=" pt-40">
         <h1 className="text-white text-5xl my-10 p-25 flex">Welcome to</h1>
         <h1 className="text-pink-300 text-9xl font-extrabold">Otaku</h1>
         <h1 className="text-pink-300 text-9xl font-extrabold">Basho</h1>
@@ -33,7 +32,7 @@ const IndexPage = () => {
             />
         </div>
       </div>
-      <StaticImage className="flex h-screen px-2 m-4 " src={"../images/Otaku2.png"} alt="otaku girl"/>
+      <StaticImage className="h-screen px-2 m-4" src={"../images/Otaku2.png"} alt="otaku girl"/>
     </main>
   )
 }
